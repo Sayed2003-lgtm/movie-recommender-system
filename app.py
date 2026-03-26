16589aa944ab7d8a6c289049e8db0a73bb604bd5
Sayed2003-lgtm <sayed.abdulkd@gmail.com>
2026-03-26 16:15:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,40 @@
 import streamlit as st
 import pickle
 import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-# Load data
+# Load dataset
 movies = pickle.load(open('movies.pkl', 'rb'))
 
-# Dummy recommend function (random movies)
-def recommend(movie):
-    return movies['title'].sample(5).values
+# Compute similarity (cached for performance)
+@st.cache_data
+def compute_similarity():
+    cv = CountVectorizer(max_features=5000, stop_words='english')
+    vectors = cv.fit_transform(movies['tags']).toarray()
+    similarity = cosine_similarity(vectors)
+    return similarity
 
-# UI
+similarity = compute_similarity()
+
+# Recommendation function
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+
+    movies_list = sorted(list(enumerate(distances)),
+                         reverse=True,
+                         key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+    for i in movies_list:
+        recommended_movies.append(movies.iloc[i[0]].title)
+
+    return recommended_movies
+
+
+# ---------------- UI ---------------- #
+
 st.title(" Movie Recommender System")
 
 selected_movie = st.selectbox(
